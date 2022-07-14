023e77f2fc05ba404598c9b1451d642eb9fa2739
Salim Hlayel <68774735+shlayel@users.noreply.github.com>
2022-07-14 21:33:16 +0400

    Add files via upload

--- apex_ebs_setup_329274.sql
+++ apex_ebs_setup_329274.sql
@@ -205,5 +205,119 @@ show errors
 
 grant execute on xx_apex_global to ^APEX;
 
+
+prompt .
+prompt ... Create View for Employee Directory
+prompt .
+
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "APPS"."XX_EMPS" ("BUSINESS_GROUP", "PERSON_ID", "IMAGE", "EMPLOYEE_NUMBER", "FIRST_NAME", "LAST_NAME", "FULL_NAME", "GENDER", "EFFECTIVE_START_DATE", "EFFECTIVE_END_DATE", "EMAIL_ADDRESS", "EMPLOYEE_LOCATION", "SUPERVISOR_EMPLOYEE_NUMBER", "SUPERVISOR_NAME", "WORK_TELEPHONE_NUMBER", "JOB_NAME", "POSITION_NAME", "DEFAULT_CHARGE_ACCOUNT", "LEDGER_NAME") AS 
+  SELECT 
+(SELECT name
+          FROM hr_all_organization_units 
+         WHERE organization_id = paaf.business_group_id
+       ) business_group,
+       papf1.person_id, 
+       i.image,
+       papf1.employee_number,
+       papf1.first_name,
+       papf1.last_name,
+       papf1.full_name,
+       papf1.sex gender,
+       papf1.effective_start_date,
+       papf1.effective_end_date,
+       papf1.email_address,
+       (SELECT hloc.location_code
+          FROM hr_locations_all hloc
+         WHERE hloc.location_id = paaf.location_id
+       ) employee_location,
+       papf2.employee_number supervisor_employee_number,
+       papf2.full_name supervisor_name,
+      (
+       SELECT pp.phone_number
+         FROM per_phones pp
+        WHERE rownum = 1 and papf1.person_id  = pp.parent_id
+          AND pp.parent_table  = 'PER_ALL_PEOPLE_F'
+          AND pp.date_from     = papf1.effective_start_date
+          AND NVL(pp.date_to,
+                  papf1.effective_end_date
+                  ) = papf1.effective_end_date
+      ) work_telephone_number,
+      (SELECT name 
+         FROM per_jobs 
+        WHERE job_id = paaf.job_id
+      ) job_name,
+      (SELECT name 
+         FROM per_all_positions
+        WHERE position_id = paaf.position_id
+      ) position_name,
+      (SELECT concatenated_segments
+         FROM gl_code_combinations_kfv 
+        WHERE code_combination_id = paaf.default_code_comb_id
+      ) default_charge_account,
+      (SELECT name 
+         FROM gl_ledgers 
+        WHERE ledger_id = paaf.set_of_books_id
+      ) ledger_name
+ FROM per_all_people_f papf1,
+      per_all_people_f papf2,
+      per_all_assignments_f paaf,
+      per_images i
+WHERE papf1.person_id = paaf.person_id
+  AND papf2.person_id = paaf.supervisor_id
+  AND paaf.primary_flag  = 'Y'
+  AND papf1.current_employee_flag = 'Y'
+  AND papf2.current_employee_flag = 'Y'
+  and papf1.person_id = i.parent_id
+  and paaf.effective_start_date = (select max(ia.effective_start_date) from per_all_assignments_f ia where ia.person_id = papf1.person_id )
+  and papf1.effective_start_date = (select max(ia.effective_start_date) from per_all_people_f ia where ia.person_id = papf1.person_id )
+   and papf2.effective_start_date = (select max(ia.effective_start_date) from per_all_people_f ia where ia.person_id = papf2.person_id );
+
+grant select on xx_emps to ^APEX;
+
+prompt .
+prompt ... Create View for Customer Locations
+prompt .
+
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "APPS"."XX_CUSTOMER_LOCATIONS" ("CUSTOMER_NUMBER", "CUSTOMER_NAME", "SITE_NUMBER", "ADDRESS1", "ADDRESS2", "ADDRESS3", "ADDRESS4", "CITY", "POSTAL_CODE", "STATE", "COUNTRY", "BILL_TO_LOCATION", "SHIP_TO_LOCATION", "GEOMETRY") AS 
+  SELECT  hca.account_number customer_number,
+                hp.party_name customer_name,
+                hps.party_site_number site_number,
+                hl.address1 address1,
+                hl.address2 address2, hl.address3 address3,
+                hl.address4 address4, hl.city city,
+                hl.postal_code postal_code, hl.state state,
+                ftt.territory_short_name country,
+                hcsua1.LOCATION bill_to_location,
+                hcsua2.LOCATION ship_to_location,
+                hl.geometry
+           FROM hz_parties hp,
+                hz_party_sites hps,
+                hz_cust_accounts hca,
+                hz_cust_acct_sites_all hcasa1,
+                hz_cust_site_uses_all hcsua1,
+                hz_locations hl,
+                fnd_territories_tl ftt,
+                hz_cust_acct_sites_all hcasa2,
+                hz_cust_site_uses_all hcsua2
+          WHERE 
+            hp.party_id = hps.party_id(+)
+            AND hp.party_id = hca.party_id(+)
+            AND hcasa1.party_site_id(+) = hps.party_site_id
+            AND hcasa2.party_site_id(+) = hps.party_site_id
+            AND hcsua1.cust_acct_site_id(+) = hcasa1.cust_acct_site_id
+            AND hcsua2.cust_acct_site_id(+) = hcasa2.cust_acct_site_id
+            AND hcsua1.site_use_code(+) = 'bill_to'
+            AND hcsua2.site_use_code(+) = 'ship_to'
+            AND hcasa1.org_id(+) = fnd_profile.VALUE ('org_id')
+            AND hcasa2.org_id(+) = fnd_profile.VALUE ('org_id')
+            AND hps.location_id = hl.location_id
+            AND hl.country = ftt.territory_code
+            AND ftt.LANGUAGE = USERENV ('lang')
+            and hl.geometry is not null;
+
+grant select on xx_customer_locations to ^APEX;
+
 spool off
 exit;
